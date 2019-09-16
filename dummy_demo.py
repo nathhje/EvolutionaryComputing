3f2c16434be0c4a0fadf61d2622e41e11c5d18bc
nathhje <n.van.sburg@hotmail.nl>
2019-09-16 13:40:54 +0200

    dummydemo in functie

--- dummy_demo.py
+++ dummy_demo.py
@@ -18,44 +18,44 @@ experiment_name = 'dummy_demo'
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
-N = 20 #Population size
 
-
-
-numbergen = 30
-growth = []
-
-# Opent random start population
-alldata = genrandomstart(N)
-
-for j in range(numbergen):
-    listscores = []
-    genfitness = 0
-
-    # check for the entire population the fitness
-    for index, data in enumerate(alldata):
-        fitness = fitnesscheck(data)
-        listscores.append([index, fitness])
-        genfitness += fitness
-
-    print("De "+ str(j) + "e generatie heeft een gemiddelde score van ", genfitness/N)
-
-    # Make N new individuals for the next generation
-    newpopulation = []
-    for i in range(N):
-        # Kiest twee parents tournament style
-        win1, win2 = pickparents(N, listscores)
-        # Crossbreed een nieuw child
-        newpopulation.append(cross(alldata[win1], alldata[win2]))
-
-    alldata = newpopulation
-    growth.append(genfitness/N)
-
-print(growth)
-plt.plot(growth)
-plt.show()
-
-with open("endresult.txt", "w") as txt_file:
-    for data in alldata:
-        for line in data:
-            txt_file.write(" ".join(str(line)) + "\n")
+def main(popsize,gens):
+    growth = []
+    
+    # Opent random start population
+    alldata = genrandomstart(popsize)
+    
+    for j in range(gens):
+        listscores = []
+        genfitness = 0
+    
+        # check for the entire population the fitness
+        for index, data in enumerate(alldata):
+            fitness = fitnesscheck(data)
+            listscores.append([index, fitness])
+            genfitness += fitness
+    
+        print("De "+ str(j) + "e generatie heeft een gemiddelde score van ", genfitness/popsize)
+    
+        # Make N new individuals for the next generation
+        newpopulation = []
+        for i in range(popsize):
+            # Kiest twee parents tournament style
+            win1, win2 = pickparents(popsize, listscores)
+            # Crossbreed een nieuw child
+            newpopulation.append(cross(alldata[win1], alldata[win2]))
+    
+        alldata = newpopulation
+        growth.append(genfitness/popsize)
+    
+    print(growth)
+    plt.plot(growth)
+    plt.show()
+    
+    with open("endresult.txt", "w") as txt_file:
+        for data in alldata:
+            for line in data:
+                txt_file.write(" ".join(str(line)) + "\n")
+            
+if __name__ == '__main__':
+    main(20,30)
