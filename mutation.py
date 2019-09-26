3a7c10cdee288d20702255bb3a6fc9dae390d93c
Julia <juliaanten@hotmail.com>
2019-09-26 16:11:16 +0200

    added randomness to mutation.py

--- mutation.py
+++ mutation.py
@@ -1,17 +1,30 @@
 import random
+import numpy as np
 
-def mutate(individual):
-    number_of_mutations = 15
+def mutate(individual, mutation_rate):
 
-    which_genes = random.sample(range(len(individual)), number_of_mutations)
 
-    for gene in which_genes:
+    mutation_decision = random.randint(0, 100)
 
-        which_action = random.randint(0,5)
-        
-        if individual[gene][which_action] == 0:
-            individual[gene][which_action] = 1
+    # krijgt een individu een mutatie
+    if mutation_decision < mutation_rate * 100:
 
-        else:
-            individual[gene][which_action] = 0
+        number_of_genes = 1500
+
+        # hoeveel mutaties krijgt een individu
+        mut_rate_calc = np.random.normal((number_of_genes * mutation_rate), 5, 1)
+
+        # welke genen (regels) worden er aangepast
+        which_genes = random.sample(range(len(individual)), mut_rate_calc)
+
+        for gene in which_genes:
+
+            # welke actie in het gen wordt aangepast
+            which_action = random.randint(0,4)
+            
+            if individual[gene][which_action] == 0:
+                individual[gene][which_action] = 1
+
+            else:
+                individual[gene][which_action] = 0
         
