430f8a65b060d5ba6be115f3056054429adf7617
nathhje <n.van.sburg@hotmail.nl>
2019-10-04 18:18:17 +0200

    mijn relevante files

---
+++ my_smart_agent.py
@@ -0,0 +1,137 @@
+################################
+# EvoMan FrameWork - V1.0 2016 #
+# Author: Karine Miras         #
+# karine.smiras@gmail.com      #
+################################
+
+# imports framework
+import sys, os
+sys.path.insert(0, 'evoman')
+from environment import Environment
+from ai_controller import player_controller
+from operator import itemgetter
+from smart_functions import *
+import random
+import matplotlib.pyplot as plt
+
+# Gene: controllers
+# Individual: single list of controllers
+# Population: collection of controllers/individuals
+
+# 1. Create the population
+# 2. Determine fitness
+# 3. Select the mating pool
+# 4. Breed
+# 5. Mutate
+# 6. Repeat
+
+
+### START ###
+experiment_name = 'dummy_demo'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+# def nextgeneration(current_generation, mutation_rate):
+    # pop_ranked = rank_scores(current_generation) #2
+
+# The genetic algorithm
+def main(pop_size, mutation_rate, nr_generations, parentchoice="Tournament", enemy = 1):
+    growth = []
+    generation = 0
+
+    # 1. Create the population
+    population = initial_population(pop_size)
+
+    
+    while generation < nr_generations:
+        if generation > 3 and sum(np.diff((growth[generation - 3], growth[generation - 2], growth[generation-1]))) == 0:
+            break
+
+        # population = nextgeneration(population, mutation_rate)
+        listscores = []
+        genfitness = 0
+
+        # 2. Determine fitness
+        # check for the entire population the fitness
+        for index, data in enumerate(population):
+            fitness = fitnesscheck(data, enemy = [enemy])
+            if fitness > 90:
+                save_good_result(data,fitness,generation,parentchoice,mutation_rate,enemy)
+            listscores.append([index, fitness])
+            genfitness += fitness
+
+        print("De "+ str(generation) + "e generatie heeft een gemiddelde score van ", genfitness/pop_size)
+
+
+        # Make N new individuals for the next generation
+        newpopulation = []
+        if parentchoice != "Tournament":
+            listscores = cumsort(listscores)
+
+        for i in range(int(pop_size/2)):
+            # 3. Select the mating pool
+            # Kiest twee parents tournament style
+            if parentchoice == "Tournament":
+                win1, win2 = pickparents(pop_size, listscores)
+            else:
+                win1, win2 = pickparentscumsort(listscores)
+
+            # 4. Breed
+            # Crossbreed een nieuw child
+            child1,child2 = cross(population[win1], population[win2])
+            newpopulation.append(child1)
+            newpopulation.append(child2)
+            #print("nieuwe ronde, nieuwe kansen")
+            #print(win1, win2)
+
+            # 5. Mutate
+            mutate(newpopulation[i], mutation_rate)
+
+        population = newpopulation
+        growth.append(genfitness/pop_size)
+
+        generation = generation + 1
+
+    # Save data
+    print(growth)
+    #save_data(growth, population, parentchoice, mutation_rate)
+    return growth
+
+def save_data(growth,parentchoice,mutation_rate,enemy):
+    
+    with open("growthresults/growthresults"+ parentchoice+ "level"+ str(enemy) +"mutation"+ str(mutation_rate) +".txt", "w") as txt_file:
+        
+        for row in growth:
+            txt_file.write(str(row)+"\n")
+
+    
+def save_good_result(data,fitness,generation,parentchoice,mutation_rate,enemy):
+    
+    with open("bestresults/bestresultgeneration" + str(generation) + "fitness"+ str(fitness) + parentchoice+ "level"+ str(enemy) +"mutation"+ str(mutation_rate) +".txt", "w") as txt_file:
+        for line in data:
+            txt_file.write(" ".join(str(line)) + "\n")
+
+#####
+# Run the genetic algorithm
+if __name__ == '__main__':
+    
+    selection="Density" #Tournament or Density
+    m_rate=0.03 # 0.005, 0.01, 0.015, 0.02, 0.025, 0.03
+    level = 1 # 1, 6, 8
+
+    hist = []
+    thegrowths = []
+    for i in range(5):
+        
+        if i%5 ==0:
+            save_data(thegrowths,selection,m_rate,level)
+        print(i)
+        thegrowths.append(main(pop_size=40, mutation_rate=m_rate, nr_generations=60, parentchoice=selection, enemy = level))
+        hist.append(thegrowths[-1][-1])
+
+    save_data(thegrowths,selection,m_rate,level)
+    
+    
+    
+    plt.hist(hist)
+    plt.show()
--- show_results_boxplot.py
+++ show_results_boxplot.py
@@ -27,7 +27,6 @@ def main(parentchoice,enemy, mutation_rate):
 def open_data(filename):
     # The growth results
 
-
     with open(filename, "r") as txt_file:
         results = txt_file.read().split('\n')
 
@@ -50,8 +49,8 @@ def end_boxplot(level):
     
     lastresult = []
     
-    for mutate in mutation:
-        for select in selection:
+    for select in selection:
+        for mutate in mutation:
             
             lastresult.append([])
             
@@ -63,9 +62,24 @@ def end_boxplot(level):
                 lastresult[-1].append(row[-1])
                 
     plt.figure()
-    plt.boxplot(lastresult, positions = [4,5,9,10,14,15,19,20,24,25,29,30], widths = 1)
+    box1 = plt.boxplot(lastresult[0:6], positions = [4,9,14,19,24,29], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box1[element], color='red')
+    for patch in box1['boxes']:
+        patch.set(facecolor='white')  
+    box2 = plt.boxplot(lastresult[6:12], positions = [5,10,15,20,25,30], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box2[element], color='green')
+    for patch in box2['boxes']:
+        patch.set(facecolor='white')  
     plt.xticks([5,10,15,20,25,30],['5','10','15','20','25','30'])
     plt.xlim(3,31)
+    plt.legend([box1["boxes"][0], box2["boxes"][0]], ['tournament', 'fitness proportion'], loc='upper right')
+    plt.xlabel(r"mutation rate ($10^{-3}$)")
+    plt.ylabel("fitness score")
+    plt.title("boxplot of the end results of level %i"%(level))
     plt.show()
     
 def best_boxplot(level):
@@ -73,10 +87,63 @@ def best_boxplot(level):
     mutation = [0.005,0.01,0.015,0.02,0.025,0.03]
     selection = ["Tournament","Density"]
     
-    lastresult = []
+    bestresult = []
     
-    for mutate in mutation:
-        for select in selection:
+    for select in selection:
+        for mutate in mutation:
+            
+            bestresult.append([])
+            
+            filename = "growthresults/growthresults"+ select + "level"+ str(level) +"mutation"+ str(mutate) +".txt"
+            
+            result = open_data(filename)
+            
+            for row in result:
+                bestresult[-1].append(max(row))
+    
+    plt.figure()
+    box1 = plt.boxplot(bestresult[0:6], positions = [4,9,14,19,24,29], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box1[element], color='red')
+    for patch in box1['boxes']:
+        patch.set(facecolor='white')  
+    box2 = plt.boxplot(bestresult[6:12], positions = [5,10,15,20,25,30], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box2[element], color='green')
+    for patch in box2['boxes']:
+        patch.set(facecolor='white')  
+    plt.xticks([5,10,15,20,25,30],['5','10','15','20','25','30'])
+    plt.xlim(3,31)
+    plt.legend([box1["boxes"][0], box2["boxes"][0]], ['tournament', 'fitness proportion'], loc='upper right')
+    plt.xlabel(r"mutation rate ($10^{-3}$)")
+    plt.ylabel("fitness score")
+    plt.title("boxplot of the best results of level %i"%(level))
+    plt.show()
+    
+def best_end_boxplot(level):
+    
+    mutation = [0.005,0.01,0.015,0.02,0.025,0.03]
+    selection = ["Tournament","Density"]
+    
+    lastresult = []
+    bestresult = []
+    
+    for select in selection:
+        for mutate in mutation:
+            
+            bestresult.append([])
+            
+            filename = "growthresults/growthresults"+ select + "level"+ str(level) +"mutation"+ str(mutate) +".txt"
+            
+            result = open_data(filename)
+            
+            for row in result:
+                bestresult[-1].append(max(row))
+                
+    for select in selection:
+        for mutate in mutation:
             
             lastresult.append([])
             
@@ -85,12 +152,40 @@ def best_boxplot(level):
             result = open_data(filename)
             
             for row in result:
-                lastresult[-1].append(max(row))
+                lastresult[-1].append(row[-1])
     
     plt.figure()
-    plt.boxplot(lastresult, positions = [4,5,9,10,14,15,19,20,24,25,29,30], widths = 1)
+    box1 = plt.boxplot(lastresult[0:6], positions = [4,9,14,19,24,29], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box1[element], color='red')
+    for patch in box1['boxes']:
+        patch.set(facecolor='white')  
+    box2 = plt.boxplot(lastresult[6:12], positions = [5,10,15,20,25,30], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box2[element], color='green')
+    for patch in box2['boxes']:
+        patch.set(facecolor='white')  
+    box3 = plt.boxplot(bestresult[0:6], positions = [4,9,14,19,24,29], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box3[element], color='cyan')
+    for patch in box3['boxes']:
+        patch.set(facecolor='white')  
+    box4 = plt.boxplot(bestresult[6:12], positions = [5,10,15,20,25,30], widths = 1, patch_artist = True)
+    #plt.setp(thebox['medians'],color = 'green')
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(box4[element], color='orange')
+    for patch in box4['boxes']:
+        patch.set(facecolor='white') 
     plt.xticks([5,10,15,20,25,30],['5','10','15','20','25','30'])
     plt.xlim(3,31)
+    plt.ylim(0,140)
+    plt.legend([box1["boxes"][0], box2["boxes"][0], box3["boxes"][0], box4["boxes"][0]], ['tournament last', 'fitness proportion last', 'fitness proportion best', 'fitness proportion best'], loc='upper right')
+    plt.xlabel(r"mutation rate ($10^{-3}$)")
+    plt.ylabel("fitness score")
+    plt.title("boxplot of the best and last results of level %i"%(level))
     plt.show()
 
 # SHOW RESULTS
@@ -137,9 +232,11 @@ if __name__ == '__main__':
 
     #selection="Density" # Tournament or Density
     #m_rate=0.015 # 0.005, 0.01, 0.015, 0.02, 0.025, 0.03
-    enemy = 1 # 1, 6, 8
+    enemy = 8 # 1, 6, 8
 
     end_boxplot(enemy)
+    best_boxplot(enemy)
+    best_end_boxplot(enemy)
 
     #data = [[9.720214626708028, 12.402528493096131, 12.414371371849523, 16.01357500503327, 20.971015634826983, 22.136871184578357, 27.758586825478364, 33.157305594949705, 35.62339327392489, 37.17300793313294, 37.87685950817044, 40.790368210598544, 43.514149170390375, 47.537660459039465, 46.67916719182411, 49.62160035753268, 49.19076399721417, 55.488303068356764, 53.47019094043301, 56.39027643336708, 57.97438190444035, 57.524775247564016, 61.84191699410119, 62.78453615662202, 63.37308573974715, 65.73765562724984, 66.86077765968066, 69.64786958156702, 72.65437310197584, 70.96542245619843, 73.95390883213852, 77.98307173612025, 78.10778245107025, 79.5803116861247, 77.07006257643361, 80.21932769948026, 81.44604523018477, 83.56758977871652, 84.50034414009414, 72.36842188753705, 68.09355175795898, 70.56881354045566, 73.04486067044239, 57.74564601793244, 59.09721671291253, 61.123263842899256, 61.12535810287271, 63.6006198853694, 64.95245236284617, 66.30245236284617, 66.75271414534285, 68.55297592783953, 70.12797592783953, 73.05297592783953, 74.62797592783953, 75.52797592783953, 75.30297592783953, 66.75297592783953, 66.75297592783953, 66.75297592783953], [5.666013216255642, 7.945724793936156, 9.090741874959344, 9.31936068026037, 12.903203774034031, 14.909217182761058, 14.932087875640656, 16.951184858902874, 15.824161611567934, 12.67874873647601, 16.9447671336736, 17.610276601786016, 14.942288245028013, 20.12001071715938, 21.487442269277942, 24.146735497186487, 28.212273717767424, 29.554599610788898, 31.3295038578483, 30.629781030383178, 32.433963107563194, 35.85484876031243, 37.865236871475716, 36.29569759399083, 39.224278574634496, 39.4681628963002, 41.27032480482909, 40.82932678052323, 43.09694330190446, 44.90447641918247, 45.567506184288526, 44.90318712753764, 45.78701659633826, 49.635009545178875, 50.9749920095507, 53.87811145544913, 54.79050818924564, 49.16674110258627, 52.30589942379396, 54.07658905902247, 53.63537379628848, 45.53967510658761, 50.70581512698643, 59.041744376772726, 62.201514225344944, 65.33352674616236, 64.83314388113737, 67.37507383243432, 67.37909176985393, 68.73144454818427, 70.08259041159286, 72.33259041159286, 73.23259041159287, 74.13259041159287, 75.70669517527544, 75.70669517527544, 75.25669517527544, 74.35848564791027, 75.25669517527544]]
 
