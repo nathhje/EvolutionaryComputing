e9130014ae0dcbb06aedc4e4559fe8beb2bc603b
Julia <juliaanten@hotmail.com>
2019-10-04 14:18:50 +0200

    added run_results

---
+++ run_results.py
@@ -0,0 +1,34 @@
+import sys, os
+sys.path.insert(0, 'evoman')
+from environment import Environment
+from ai_controller import player_controller
+from operator import itemgetter
+from smart_functions import *
+import random
+import matplotlib.pyplot as plt
+
+index = 0
+
+experiment_name = 'dummy_demo'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+import glob
+import errno
+path = '/Users/Julia/Dropbox/Master/Evolutionary computing/EvolutionaryComputing/bestresults/bestresultsDensitylevel8mutation0.025/*.txt'
+files = glob.glob(path)
+
+for file in files:
+
+	index += 1
+	f = open(file, "r")
+	copy = open("output.txt", "w")
+	for line in f:
+	    copy.write(line)
+
+# initializes environment with ai player using random controller, playing against static enemy
+	env = Environment(experiment_name=experiment_name, player_controller=player_controller(), speed="fastest", enemies = [8])
+	alle = env.play()
+
+	with open("run/Densitylevel8mutation0.025.txt", "a") as txt_file:
+		txt_file.write(str(index) + ": " + str(alle) + "\n")
